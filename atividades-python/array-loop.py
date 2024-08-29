662fc88fa2812e4ab72cad8e47fdb9c1ba60082b
João Lee <joaumlimaum@gmail.com>
2024-08-29 15:38:01 -0300

    Correção - exercício arrays-loop.py - adicionei só a listagem sem a frase do desafio"

--- atividades-python/array-loop.py
+++ atividades-python/array-loop.py
@@ -5,13 +5,23 @@
 lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
 
 lista_produtos_tamanho = len(lista_produtos)
+contador_um = 0
+contador_dois = 0
 
 # Com laço `while`:
-contador = 0
-while contador < lista_produtos_tamanho:
-  print(f"Temos {lista_produtos[contador]} à venda!")
-  contador += 1
+while contador_um < lista_produtos_tamanho:
+  print(lista_produtos[contador_um])
+  contador_um +=1
+
+# Com laço `while` e com a frase pedida:
+while contador_dois < lista_produtos_tamanho:
+  print(f"Temos {lista_produtos[contador_dois]} à venda!")
+  contador_dois += 1
 
 # Com laço `for`:
+for index in range(lista_produtos_tamanho):
+  print(lista_produtos[index])
+
+# Com laço `for` com a frase pedida:
 for index in range(lista_produtos_tamanho):
   print(f"Temos {lista_produtos[index]} à venda!")
